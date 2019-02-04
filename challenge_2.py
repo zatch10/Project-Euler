54980b6a4a077adf4f47808587cdb311b9a84ce2
Chirag Nanda <chiragsnanda@gmail.com>
2019-02-04 01:33:23 -0600

    challenge_2 complete

---
+++ challenge_2.py
@@ -0,0 +1,23 @@
+#challenge_2#
+fibb1 = 1
+fibb2 = 2
+sum = 2
+limit = 0
+count = 0
+while limit<=4000000:
+    fibb2 += fibb1
+    fibb1 = fibb2 - fibb1
+    limit = fibb1
+    count += 1
+
+fibb1 = 1
+fibb2 = 2
+for i in range(0,count):
+    fibb2 += fibb1
+    fibb1 = fibb2 - fibb1
+    if fibb2%2 == 0:
+        sum += fibb2
+    i += 1
+print(sum)
+    
+
